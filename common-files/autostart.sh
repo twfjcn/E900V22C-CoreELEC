2ca651095294a1a38c2940dd8183277a9a7deb2c
twfjcn <127495012+twfjcn@users.noreply.github.com>
2025-03-22 03:15:02 +0800

    Update autostart.sh

--- common-files/autostart.sh
+++ common-files/autostart.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-systemctl start bluetooth
-#sleep表示等待时间 时间长短根据电视开机时间调整 默认3秒足够用了
-sleep 3
-cp /storage/config.toml /storage/Waytech/CloudDrive2/config.toml
-cd /storage/.kodi/clouddrive2
-./clouddrive &
 
-cd /storage/.kodi/alist/
-./alist server &
 
