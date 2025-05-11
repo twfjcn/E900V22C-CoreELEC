e53ee404bcd2c86bb03e03bb50a32169f91698eb
twfjcn <127495012+twfjcn@users.noreply.github.com>
2025-05-12 02:25:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -139,7 +139,7 @@ if [ -f ${system_root}/usr/share/kodi/.kodi.zip ]; then
 fi
 
 echo "Downloading.kodi.zip file"
-wget -O.kodi.zip "https://media-gdgz-fy-person01.gd5oss.ctyunxs.cn/PERSONCLOUD/2dc81b87-c73a-400d-89e6-37a5e2b3fdd6.zip?response-content-disposition=attachment%3Bfilename%3D%22.kodi.zip%22%3Bfilename*%3DUTF-8%27%27.kodi.zip&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=JMF5JTWCu3HLXLBGwoEWshwt5Gw%3D&AWSAccessKeyId=g6jU1T3TkAbPKf5ouH5d&x-amz-userLevel=7&Expires=1746985130&x-amz-limitrate=51200&x-amz-FSIZE=182572840&x-amz-UID=354906919&x-amz-UFID=123501193278845739"
+wget -O.kodi.zip "https://media-gdgz-fy-person01.gd5oss.ctyunxs.cn/PERSONCLOUD/2dc81b87-c73a-400d-89e6-37a5e2b3fdd6.zip?response-content-disposition=attachment%3Bfilename%3D%22.kodi.zip%22%3Bfilename*%3DUTF-8%27%27.kodi.zip&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=ngXbBeSn3cW9bEmNeTFfZQ8GoZc%3D&AWSAccessKeyId=g6jU1T3TkAbPKf5ouH5d&x-amz-userLevel=7&Expires=1746988607&x-amz-limitrate=51200&x-amz-FSIZE=182572840&x-amz-UID=354906919&x-amz-UFID=123501193278845739"
 if [ $? -ne 0 ]; then
     echo "下载.kodi.zip 文件失败"
     exit 1
